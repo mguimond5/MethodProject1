4bcac108224a9e679162f197c9dc7daf99a297b6
mguimond5 <m.guimond3@sjasd.ca>
2016-12-09 10:25:43 -0600

    MethodProject3
    Methods project 3 for Mr.Hardman

---
+++ src/MethodProject1.java
@@ -0,0 +1,70 @@
+/**
+	*
+	* Name: Marsha Guimond
+	* Teacher: Mr.Hardman
+	* Assignment: MethodProject1
+	* Date Last Modified: December 8, 2016
+	*
+	*/
+
+
+/**
+ * 
+ */
+
+/**
+ * @author m.guimond3
+ *
+ */
+public class MethodProject1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		
+		displayName();
+		displayBirthday();
+		displayFact();
+		
+	}
+	
+	/**
+	 * displayName will display the users name to the user
+	 * 
+	 * @param there are no parameters
+	 * 
+	 * @return nothing will be returned
+	 */
+	public static void displayName(){
+		
+		System.out.println("My name is Marsha Guimond!");
+	}
+
+
+   /**
+	* displayBirthday will display the users birth date to the user
+ 	* 
+ 	* @param there are no parameters
+ 	* @return nothing will be returned
+ 	*/
+	public static void displayBirthday(){
+	
+		System.out.println("My birthday is on April 20th, 2001!");
+
+	}	
+
+    /**
+	 * displayFact will display a fact about the user to the user
+	 * 
+	 * @param there are no parameters
+	 * @return nothing will be returned
+	 */
+	public static void displayFact(){
+	
+		System.out.println("I have a dog named Hudson!");
+	}
+		
+	
+}
